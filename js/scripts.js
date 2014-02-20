f7c890d8475d8162ef325888b24fdf334e657f1f
Edgar <aedgar1@engr-131-12.eastern.ewu.edu>
2014-02-20 14:23:02 -0800

    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 // Make Foundation Go!
-$(document).foundation();
+//$(document).foundation();
 
 // Your Awesome Scripts!
 $(document).ready(function(){
+
+//Good stuff
+	new Maplace({
+		show_markers: true,
+		locations: [{
+			lat: 45.9, 
+			lon: 10.9,
+			zoom: 8
+		}]
+	}).Load(); 	
 
 	console.log('Page Loaded. Lets Do this!');
 
